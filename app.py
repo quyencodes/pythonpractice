fb9ada060059b32b509c17b44cd2d5e6143641f2
Quyen Hoang <qduhoang@gmail.com>
2022-12-11 23:14:41 -0800

    Completed beginner python course

--- app.py
+++ app.py
@@ -211,3 +211,33 @@
 # employee_file.close()
 
 # Modules and Pip
+# Classes and Objects
+from Student import Student
+# Student1 = Student("Jim", "Business", 3.1, False)
+Student2 = Student("Pam", "Art", 3.8, True)
+# print(Student2.name)
+print(Student2.on_honor_roll())
+
+# question_prompts = [
+#   "What color are apples?\n(a) Red/Green\n(b) Purple\n(c) Orange\n\n",
+#   "What color are bananas?\n(a) Teal\n(b) Magenta\n(c) Yellow\n\n",
+#   "What color are strawberries?\n(a) Yellow\n(b) Red\n(c) Blue\n\n",
+# ]
+
+# from Question import Question
+
+# questions = [
+#   Question(question_prompts[0], "a"),
+#   Question(question_prompts[1], "c"),
+#   Question(question_prompts[2], "b"),
+# ]
+
+# def run_test(questions):
+#   score = 0
+#   for question in questions:
+#     answer = input(question.prompt)
+#     if answer == question.answer:
+#       score += 1
+#   print("You got " + str(score) + "/" + str(len(question_prompts)) + " correct")
+
+# run_test(questions)
